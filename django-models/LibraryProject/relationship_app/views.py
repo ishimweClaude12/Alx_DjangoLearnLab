3d7f4d7295d046fc281846b35105a2227705e09a
Claude Ishimwe <dpqb12haikuo@gmail.com>
2025-11-17 21:07:42 +0200

    fix: second task test 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Book, Library
 
 # Create your views here.
