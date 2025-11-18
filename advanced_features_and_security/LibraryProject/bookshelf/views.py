a755a8ef5070a75dc091f6ab1aa165c0b4a0e91c
Claude Ishimwe <dpqb12haikuo@gmail.com>
2025-11-18 08:39:48 +0200

    fix view.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # Create your views here.
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_list(request):
+    """
+    View to list all books.
+    Requires can_create permission.
+    """
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
